9c01c165d8a0f86afc36110a4f9e15db9037933b
GeruMansi <mangeragu@gmail.com>
2023-05-14 07:29:55 -0300

    Finished game

--- World.gd
+++ World.gd
@@ -12,25 +12,22 @@ const LEVEL_8 = preload("res://levels/level_8.tscn")
 const LEVEL_9 = preload("res://levels/level_9.tscn")
 const LEVEL_10 = preload("res://levels/level_10.tscn")
 const LEVEL_11 = preload("res://levels/level_11.tscn")
+const LEVEL_12 = preload("res://levels/level_12.tscn")
+const END = preload("res://end.tscn")
 
 @onready var music = $AudioStreamPlayer
 @onready var boop = $AudioStreamPlayer2
 @onready var down = $AudioStreamPlayer3
 @onready var restart = $AudioStreamPlayer4
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if get_tree().get_root().get_child(2).name == "Level2":
-		$Label.show()
+@onready var label1 = $Label
+@onready var label2 = $Label2
 
 func change_level(path):
-	get_tree().create_timer(.5)
+	
+	var timer1 = get_tree().create_timer(.5)
+	await timer1.timeout
 	
 	get_tree().change_scene_to_packed(path)
 	
-	get_tree().create_timer(.5)
+	var timer2 = get_tree().create_timer(.5)
+	await timer2.timeout
